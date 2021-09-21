7dc40d6ce719c90832a812aa34546cc23b5e04bb
Ananysingh212 <78842855+Ananysingh212@users.noreply.github.com>
2021-09-21 11:48:07 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "THE LAW";
+const accessCode2 = "PIANO";
+const accessCode3 = "TON";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("T L A H E W", 100,50)
+    fill("white")
+    text("Hint: Break me, and I’ll cage you. Obey me, and I’ll save you. What am I? ", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("P O N I A", 700,150)
+    fill("white")
+    text("Hint: What has many keys but can’t open a single door? ", 600,170)
+
+    fill("white")
+    textSize(15)
+    text("N O T", 100,250)
+    fill("white")
+    text("Hint: Forward it’s heavy, backward it’s not. What is it?", 100,270)
+
+}
